4a609dea92f09a333826b6d3199b421899961e09
Sloke Shrestha <slokeshrestha131@gmail.com>
2023-04-09 19:34:15 -0500

    Implemented feature extraction class which loads data and calculates IMU featues. Still need to implement audio embeddings extraction

---
+++ analysis_pipelines/dataset.py
@@ -0,0 +1,15 @@
+# PhIMU-A labels definition 
+
+labels = {
+    'portrait_hold': 0,
+    'landscape_hold': 1,
+    'tap': 2,
+    'taking_call': 3,
+    'swipe_left_to_right': 4,
+    'swipe_right_to_left': 5,
+    'scroll_bottom_to_top': 6,
+    'tap_while_on_table': 7,
+    'on_table_face_up': 8,
+    'on_table_face_down': 9,
+    'null_class': 10
+}
