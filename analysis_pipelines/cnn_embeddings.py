626a20f6171958aa56aac047784c60791369fd2d
Sloke Shrestha <slokeshrestha131@gmail.com>
2023-04-11 15:42:24 -0500

    Implemented cnn_embeddings.py

--- analysis_pipelines/cnn_embeddings.py
+++ analysis_pipelines/cnn_embeddings.py
@@ -1,56 +1,45 @@
-"""File to extract audio embeddings from a CNN model."""
+"""File to extract audio embeddings from YamNet."""
 
-import torch
-import torchaudio
 import numpy as np
-
 import tensorflow_hub as hub
 
-import torch
-import torchaudio
+# imports for testing
 from librosa import load
 
-
-YMNET_SF = 16000
-
-
-def get_audio_embeddings(audio_data, yamnet_model):
-    # Load audio data using torchaudio
-    # import pdb; pdb.set_trace()
-    waveform, sample_rate = torchaudio.load(audio_data)
-    # use librosa load instead and convert to tensor
-
+"""Class to extract 1024 audio embeddings from YamNet.
+Example:
+    >>> from cnn_embeddings import YamNetEmbeddings
+    >>> audio = librosa.load("audio_file.wav", sr=16000) # make sure to load audio at 16000 sample rate
+    >>> embeddings = YamNetEmbeddings.get_audio_embeddings(audio)
     
-    import pdb; pdb.set_trace()
-    # resample to 16kHz
-    waveform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=YMNET_SF)(waveform)
+    
+    Note: Only accepts audio files less than 0.96 seconds long."""
 
+class YamNetEmbeddings:
 
-    # Preprocess audio data for input to the YAMNet model
-    waveform = waveform.mean(dim=0, keepdim=True)
-    waveform = zero_padding(waveform, YMNET_SF)
-    mel_spectrogram = torchaudio.transforms.MelSpectrogram(sample_rate=YMNET_SF, n_fft=2048, hop_length=512, n_mels=64)(waveform)
-    mel_spectrogram = torchaudio.transforms.AmplitudeToDB()(mel_spectrogram)
-    input_data = torch.transpose(mel_spectrogram, 1, 2)
+    YMNET_SF = 16000
+    yamnet_model = hub.load('https://tfhub.dev/google/yamnet/1')
 
-    # Convert audio data to a tensor and apply YAMNet model
-    with torch.no_grad():
-        audio_embedding = yamnet_model(input_data)
+    @staticmethod
+    def get_audio_embeddings(waveform):
+        waveform = YamNetEmbeddings.__zero_padding(waveform, YamNetEmbeddings.YMNET_SF).astype(np.float32)
+        _, embeddings, _ = YamNetEmbeddings.yamnet_model(waveform)
 
-    return audio_embedding
+        return embeddings.numpy()
+    @staticmethod
+    def __zero_padding(audio, sample_rate):
+        """Zero pad audio to 0.96 seconds"""
+        audio_length = audio.shape[0]
+        audio_length = audio_length / sample_rate
+        if audio_length < 0.96:
+            audio = np.pad(audio, (0, int((0.96 - audio_length)*sample_rate)), 'constant')
+        return audio
 
-def zero_padding(audio, sample_rate):
-    """Zero pad audio to 0.96 seconds"""
-    audio_length = audio.shape[1]
-    audio_length = audio_length / sample_rate
-    if audio_length < 0.96:
-        audio = np.pad(audio, (0, int(0.96 * sample_rate) - audio_length), 'constant')
-    return audio
 
 if __name__ == "__main__":
     # Load yamnet model
-    yamnet_model = hub.load('https://tfhub.dev/google/yamnet/1')
-    audio_embedding = get_audio_embeddings("/Users/sloke"\
-                                            "/Downloads/Microphone.wav", yamnet_model)
+    waveform, sample_rate = load("dataset/Sloke/landscape_hold_2023-03-02_21-14-02/Microphone.caf", sr=YamNetEmbeddings.YMNET_SF)
+    waveform = waveform[:int(YamNetEmbeddings.YMNET_SF*0.2)]
+    embeddings = YamNetEmbeddings.get_audio_embeddings(waveform)
 
     
